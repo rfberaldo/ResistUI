358be5d5bb951bf9c35a318715b887f3f8ebcfaa
Rafael Beraldo <rafael.pimpa@gmail.com>
2024-12-23 13:23:31 -0300

    add spell name on interrupt announce

--- Modules/Interrupt.lua
+++ Modules/Interrupt.lua
@@ -5,19 +5,24 @@ function module:OnLoad()
 	local f = CreateFrame("Frame")
 	f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 	f:SetScript("OnEvent", function()
-		if not IsInGroup() and not IsInRaid() then return end
+		-- See: https://wowpedia.fandom.com/wiki/COMBAT_LOG_EVENT
+		local log = ({ CombatLogGetCurrentEventInfo() })
+		local event = log[2]
+		local sourceGUID = log[4]
+		local destName = log[9]
+		local spellId = log[15]
+		local spellName = log[16]
 
-		local _, event, _, sourceGUID, _, _, _, _, destName = CombatLogGetCurrentEventInfo()
 		if (event ~= "SPELL_INTERRUPT") then return end
 		if (sourceGUID ~= UnitGUID("player")) then return end
 
-		local chatChannel = "PARTY"
-		if IsInRaid() then
-			chatChannel = "RAID"
+		local spell = spellId > 0 and GetSpellLink(spellId) or spellName
+		local msg = "Interrupted " .. (spell or destName)
+
+		if IsInGroup() or IsInRaid() then
+			SendChatMessage(msg, IsInRaid() and "RAID" or "PARTY")
+		else
+			print("ResistUI:", msg)
 		end
-
-		local msg = "Interrupted " .. destName
-
-		SendChatMessage(msg, chatChannel)
 	end)
 end

63dacbc32734dc1de07ed6ab5930a9dd73558869
Rafael Beraldo <rafael.pimpa@gmail.com>
2025-01-13 14:38:42 -0300

    use original class icons

--- Modules/UnitFramesClassPortrait.lua
+++ Modules/UnitFramesClassPortrait.lua
@@ -4,8 +4,6 @@ local module = ResistUI:NewModule()
 function module:OnLoad()
 	if not ResistUICfg.classPortraits then return end
 
-	local texture = ResistUI.texturePath .. "ClassPortraits\\%s.tga"
-
 	local function update(self)
 		if not self.portrait then return end
 		if not UnitIsPlayer(self.unit) then return end
@@ -13,7 +11,17 @@ function module:OnLoad()
 		local _, class = UnitClass(self.unit)
 		if not class then return end
 
-		self.portrait:SetTexture(format(texture, class))
+		local classIconAtlas = GetClassAtlas(class)
+		if not classIconAtlas then return end
+
+		self.portrait:SetAtlas(classIconAtlas, false)
+
+		if self.mask then return end
+
+		self.mask = self:CreateMaskTexture()
+		self.mask:SetAllPoints(self.portrait)
+		self.mask:SetTexture("Interface/CHARACTERFRAME/TempPortraitAlphaMask")
+		self.portrait:AddMaskTexture(self.mask)
 	end
 
 	hooksecurefunc("UnitFramePortrait_Update", update)
